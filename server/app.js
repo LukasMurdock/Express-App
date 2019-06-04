64d1e078fadc5c9de65f8144e45e8912880a0807
Lukas Murdock <lukas@Lukass-MacBook-Pro.local>
2019-06-04 08:45:05 -0400

    commit

--- server/app.js
+++ server/app.js
@@ -5,11 +5,11 @@ var logger = require('morgan');
 var fs = require('fs')
 
 
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,42 +20,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-app.use(express.static('images'))
+app.get('/testt', testt);
+
+  function testt(request, response){
+    response.send("test");
+  }
 
 
-app.get('/video', function(getVideo) {
-  function getVideo(request, response){
-    var path = 'assets/first.mp4'
-    var stat = fs.statSync(path)
-    var fileSize = stat.size
-    var range = req.headers.range
+
+/*
+  app.get('/video', function(request, response){
+    var readStream = fs.createReadStream('assets/first.mp4');
   
-    if (range) {
-      var parts = range.replace(/bytes=/, "").split("-")
-      var start = parseInt(parts[0], 10)
-      var end = parts[1]
-        ? parseInt(parts[1], 10)
-        : fileSize-1
-  
-      const chunksize = (end-start)+1
-      const file = fs.createReadStream(path, {start, end})
-      const head = {
-        'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-        'Accept-Ranges': 'bytes',
-        'Content-Length': chunksize,
-        'Content-Type': 'video/mp4',
-      }
-  
-      res.writeHead(206, head)
-      file.pipe(res)
-    } else {
-      const head = {
-        'Content-Length': fileSize,
-        'Content-Type': 'video/mp4',
-      }
-      res.writeHead(200, head)
-      fs.createReadStream(path).pipe(res)
-    }
-  }})
+  });
+
+
+app.use(express.static('images'));
 
 module.exports = app;
